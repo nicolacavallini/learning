6315a88dab0082ff0eebe034117720bc6302d45e
Nicola Cavallini <n.cavallini@camlintechnologies.com>
2017-02-03 13:01:05 +0100

    read an write binary

--- cpp/write_vetor_to_bynary.cpp
+++ cpp/write_vetor_to_bynary.cpp
@@ -3,6 +3,28 @@
 
 #include <vector>
 
+template<class T>
+void write_vector_to_binary(const std::string filenmame,
+                      const std:: vector<T> &vec) {
+    std::ofstream file(filenmame, std::ios::binary);
+
+    for (const double& v : vec) file.write(reinterpret_cast<const char*>( &v )
+        ,sizeof(T));
+}
+
+template<class T>
+std::vector<T> read_binary_to_vector(const std::string filenmame){
+    std::vector<T> vec;
+
+    std::ifstream file( filenmame, std::ios::binary );
+    T v;
+
+    while( file.read( reinterpret_cast<char*>( &v ), sizeof(T) ) )
+       vec.push_back(v) ;
+
+   return vec;
+}
+
 int main()
 {
     std::cout << "stocazzo" << std::endl;
@@ -11,10 +33,13 @@ int main()
     vec.push_back(2.0);
     vec.push_back(3.0);
 
-    std::ofstream file("outfile.dat", std::ios::binary);
+    write_vector_to_binary("outfile.dat",vec);
 
-    for (const double& v : vec) file.write(reinterpret_cast<const char*>( &v )
-        ,sizeof(double));
+    std::vector<double> read =
+    read_binary_to_vector<double>("outfile.dat");
+
+    for (auto const& r: read)
+        std::cout << "value = " << r << std::endl;
 
     return 0;
 }

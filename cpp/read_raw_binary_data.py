623dcb4f5b3072e8e3a4b001a13b08d8dda64240
Nicola Cavallini <n.cavallini@camlintechnologies.com>
2017-02-03 15:51:26 +0100

    minor

--- cpp/read_raw_binary_data.py
+++ cpp/read_raw_binary_data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    f = open("outfile.dat   ", "rb")
+    f = open("outfile.dat", "rb")
     data = np.fromfile(f, dtype=np.float64)
     f.close()
 

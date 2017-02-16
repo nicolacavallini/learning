7db095f56ba57ad66a255672fb218c984d1540f9
Nicola Cavallini <n.cavallini@camlintechnologies.com>
2017-02-16 09:32:56 +0100

    vector of shared ptr of sparsity pattern

--- dealii/symmetrize.cc
+++ dealii/symmetrize.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+#include <memory>
 
 #include <deal.II/lac/sparsity_pattern.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
@@ -17,29 +20,41 @@ DynamicSparsityPattern from_static_to_dynamic(SparsityPattern & sp){
 }
 
 int main(){
-    cout << "stocazzo" << endl;
+
     DynamicSparsityPattern dsp(5,5);
 
     dsp.add(3,2);
 
+    shared_ptr<SparsityPattern> sp(new SparsityPattern());
 
-    SparsityPattern sp;
-    sp.copy_from(dsp);
-
+    sp->copy_from(dsp);
+    sp->compress();
 
     ofstream myfile;
     myfile.open ("sp.svg");
-    sp.print_svg(myfile);
+    sp->print_svg(myfile);
     myfile.close();
 
-    DynamicSparsityPattern dsp2 = from_static_to_dynamic(sp);
+    vector<shared_ptr<SparsityPattern>> vec_sp;
 
+    vec_sp.push_back(sp);
+    cout << "vec_sp[0] = " << vec_sp[0]->n_nonzero_elements() << endl;
+
+    DynamicSparsityPattern dsp2 = from_static_to_dynamic(*sp);
     dsp2.symmetrize();
-    sp.copy_from(dsp2);
+
+    shared_ptr<SparsityPattern> sp1(new SparsityPattern());
+    sp1->copy_from(dsp2);
 
     myfile.open ("sym_sp.svg");
-    sp.print_svg(myfile);
+    sp1->print_svg(myfile);
     myfile.close();
+
+    vec_sp.push_back(sp1);
+
+    cout << "vec_sp[1] = " << vec_sp[1]->n_nonzero_elements() << endl;
+
+    cout << "stocazzo" << endl;
 
     return 0;
 }

211f8deff32b3bfe036d527cb9cfce446163139e
Nicola Cavallini <n.cavallini@camlintechnologies.com>
2017-02-27 15:34:58 +0100

    added lambda function

--- cpp/map_of_funcs.cpp
+++ cpp/map_of_funcs.cpp
@@ -17,6 +17,7 @@ map<string,function<void()>> map_funcs_to_strings(){
     map<string,function<void()>> string_to_func;
     string_to_func["ciao"] = write_ciao;
     string_to_func["hello"] = write_hello;
+    string_to_func["ciao_l"] = [](){cout << "ciao from labda" << endl;};
 
     return string_to_func;
 }
@@ -28,6 +29,8 @@ int main(){
 
     what_to_write["ciao"]();
     what_to_write["hello"]();
+    what_to_write["ciao_l"]();
+
 
     return 0;
 }

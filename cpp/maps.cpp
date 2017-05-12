ab13999b64d5f4fef193d00c31be31fae0366e1e
Nicola Cavallini <n.cavallini@camlintechnologies.com>
2017-05-12 14:49:47 +0200

    maps examples

--- cpp/maps.cpp
+++ cpp/maps.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <array>
 
 using namespace std;
 
@@ -12,14 +13,29 @@ using namespace std;
 
 
 namespace test{
-    map<string,vector<double>> mappa = {{"here",{0}},{"we",{1}},{"go",{2}}};
+    map<string,vector<double>> mappa_vec = {{"here",{0,2}},{"we",{1,5}},{"go",{2,9}}};
+    map<string,array<double,2>> mappa_arr = {{"here",{0,2}},{"we",{1,5}},{"go",{2,9}}};
+}
+
+void print_map(){
+    cout << "this is a good spot for a template exercise" << endl;
 }
 
 int main(){
 
-    auto it = ::test::mappa.begin();
-    for (; it!=::test::mappa.begin(); ++it){
-        cout << "map = "<< it->first  << endl; //<< ", "<< it->second <<endl;
+    cout << "stocazzo" << endl;
+
+    auto it = ::test::mappa_vec.begin();
+    for (; it!=::test::mappa_vec.end(); ++it){
+        cout << "map = "<< it->first  << ", "
+             << it->second[0] << ", "<< it->second[1] << endl;
     }
+
+    auto it_ = ::test::mappa_arr.begin();
+    for (; it_!=::test::mappa_arr.end(); ++it_){
+        cout << "map = "<< it_->first  << ", "
+             << it_->second[0] << ", "<< it_->second[1] << endl;
+    }
+
     return 0;
 }

86801d5040891e79e203c8e0a5a31d878cd7e646
Nicola Cavallini <n.cavallini@camlintechnologies.com>
2017-07-19 16:43:42 +0200

    cpp style converging

--- mixture_models_gaussian/cpp-style/mixed_gaussian.cpp
+++ mixture_models_gaussian/cpp-style/mixed_gaussian.cpp
@@ -6,11 +6,9 @@
 #include <iostream>
 #include <iomanip>
 
-typedef double data_t;
+typedef float data_t;
 
 using namespace std;
-
-typedef double data_t;
 
 template<class T>
 void print_vec(const vector<T> &vec)
@@ -22,7 +20,33 @@ void print_vec(const vector<T> &vec)
     cout << "]"<< endl;
 }
 
-data_t get_gaussian_random(data_t mean, data_t std) {
+template<class T>
+void print_vec_vec(vector<vector<T> > &vec)
+{
+    for (const auto &row : vec){
+        for (const auto &el : row){
+            cout << el << ", ";
+        }
+        cout << "stocazzo"<<  endl;
+    }
+}
+
+template<class T>
+vector<vector<T> > init_vec_vec(const int rows,
+                                const int cols)
+{
+    vector<vector<T> > vec(rows,vector<T>(cols));
+
+    for (int i = 0; i <rows; i++){
+        for (int j =0; j < cols; j++){
+            vec[i][j] = 0;
+        }
+
+    }
+    return vec;
+}
+
+data_t get_gaussian_random(const data_t mean, const data_t std) {
     std::random_device rd;
     std::mt19937 e2(rd());
     std::normal_distribution<data_t> gen(mean, std);
@@ -30,18 +54,33 @@ data_t get_gaussian_random(data_t mean, data_t std) {
     return gen(e2);
 }
 
-data_t* generate_1d_data(data_t mean, data_t std, int count) {
-    data_t* data = new data_t[count];
-    for(int i=0;i<count;i++) {
+vector<data_t> generate_1d_data(const data_t mean,
+                                const data_t std,
+                                const int count) {
+    vector<data_t> data(count);
+    for(int i=0;i<data.size();i++) {
         data_t noise = 2*std * (data_t)rand()/RAND_MAX;
-        //noise -= 10;
         data[i] = get_gaussian_random(mean, std)+noise;
     }
 
     return data;
 }
 
-data_t* get_data_means_var(data_t* data, int count_per_component, int num_components) {
+vector<data_t>get_data_means_var(
+        const int num_components) {
+
+    vector<data_t> ret(2*num_components);
+
+    for(int i=0;i<2*num_components;i+=2) {
+        ret[i] = 640*i/(2.0*num_components) + 120;
+        ret[i+1] = 1000;
+    }
+
+    return ret;
+}
+
+
+data_t* get_data_means_var(data_t* data, int samples_per_component, int num_components) {
     data_t* ret = new data_t[2*num_components];
 
     for(int i=0;i<2*num_components;i+=2) {
@@ -52,11 +91,14 @@ data_t* get_data_means_var(data_t* data, int count_per_component, int num_compon
     return ret;
 }
 
-data_t calculate_1d_probability(data_t value, data_t mean, data_t std) {
-    data_t ret = 0.0f;
+data_t calculate_1d_probability(
+        const data_t value,
+        const data_t mean,
+        const data_t std) {
+    data_t ret = 0.0;
 
-    data_t frac = 1.0f / (std * sqrt(2 * M_PI));
-    data_t power = -0.5f * pow((value - mean) / std, 2.0);
+    data_t frac = 1.0 / (std * sqrt(2 * M_PI));
+    data_t power = -0.5 * pow((value - mean) / std, 2.0);
 
     ret = frac * exp(power);
 
@@ -65,7 +107,7 @@ data_t calculate_1d_probability(data_t value, data_t mean, data_t std) {
 
 void draw_1d_data_mixture(cv::Mat img,
                           data_t* data, data_t* weights,
-                          int count_per_component, int num_components){
+                          int samples_per_component, int num_components){
     std::vector<cv::Scalar> palette;
     palette.push_back(cv::Scalar(0, 0, 255));   // red
     palette.push_back(cv::Scalar(255, 0, 0));
@@ -73,8 +115,8 @@ void draw_1d_data_mixture(cv::Mat img,
     palette.push_back(cv::Scalar(0, 0, 0));
     palette.push_back(cv::Scalar(192, 192, 192));
 
-    int total = count_per_component * num_components;
-    for(int i=0;i<total;i++) {
+    int num_samples = samples_per_component * num_components;
+    for(int i=0;i<num_samples;i++) {
         data_t* wt = &weights[i*num_components];
 
         cv::Scalar color = cv::Scalar(0);
@@ -130,75 +172,103 @@ void draw_1d_gaussian_mixture(cv::Mat img, data_t* mean, data_t* stddev, data_t*
 }
 
 void run_gaussian_mixture_1d() {
-    const int num_components = 5;
-    const int count_per_component = 100;
-    data_t *data = new data_t[num_components*count_per_component];
+    //const int num_components = 5;
 
-    data_t mean[5] = {120, 480, 240, 360, 320};
-    data_t std[5] = {20, 30, 10, 40, 5};
 
-    for(int i=0;i<num_components;i++) {
-        data_t* new_data = generate_1d_data(mean[i], std[i], count_per_component);
-        memcpy(data+(count_per_component*i), new_data, sizeof(data_t)*count_per_component);
+    //vector<data_t> data(num_components*samples_per_component);
+
+    vector<data_t> mean = {120, 480, 240, 360, 320};//{120, 480};//
+    vector<data_t> std = {20, 30, 10, 40, 5};//{20, 30};//
+    int num_components = mean.size();
+
+
+    const int samples_per_component = 100;
+    vector<data_t> data;
+
+    for(int i=0; i<mean.size();i++){
+        for (int j=0 ; j< samples_per_component ; j++){
+            data.push_back(get_gaussian_random(mean[i],std[i]));
+        }
     }
+        //data[i] = generate_1d_data(mean[i], std[i], samples_per_component);
 
-    data_t* initial_guess = get_data_means_var(data, count_per_component, num_components);
-    int total = num_components * count_per_component;
-    data_t* responsibility = new data_t[num_components*total];
-    data_t* pi             = new data_t[num_components];
-    data_t* learned_means  = new data_t[num_components];
-    data_t* learned_std    = new data_t[num_components];
+
+    //print_vec(data);
+
+
+    vector<data_t> initial_guess = get_data_means_var(
+                num_components);
+
+    //print_vec(initial_guess);
+
+
+    int num_samples = num_components * samples_per_component;
+    //data_t* responsibility = new data_t[num_components*num_samples];
+
+    vector<vector<data_t>> responsibility =
+            init_vec_vec<data_t>(num_components,num_samples);
+            //responsibility(num_components,
+            //vector<data_t>(num_samples));
+    vector<data_t> pi (num_components);
+    vector<data_t> learned_means (num_components);
+    vector<data_t> learned_std(num_components);
 
     for(int i=0;i<num_components;i++) {
         learned_means[i] = initial_guess[2*i];
         learned_std[i] = sqrt(initial_guess[2*i+1]);
-        pi[i] = 1.0f/num_components;
+        pi[i] = 1.0/num_components;
     }
 
-    for(int iter=0;iter<100;iter++) {
+    for(int iter=0;iter<101;iter++) {
         // E step
-        for(int i=0;i<total;i++) {
+        for(int i=0;i<num_samples;i++) {
             data_t total_prob = 0.0f;
             for(int j=0;j<num_components;j++) {
                 data_t prob = pi[j] *
                         calculate_1d_probability(
                             data[i], learned_means[j], learned_std[j]);
                 total_prob += prob;
-                responsibility[i*num_components+j] = prob;
+                responsibility[j][i] = prob;
             }
 
             if(total_prob > 0) {
                 for(int j=0;j<num_components;j++) {
-                    responsibility[i*num_components + j] /= total_prob;
+                    responsibility[j][i] /= total_prob;
                 }
             }
         }
+
+        //cout << responsibility.size() << endl;
+        //cout << responsibility[0].size() << endl;
+
+        //print_vec_vec(responsibility);
+
         // M step
-        data_t* nc = new data_t[num_components];
-        memset(nc, 0, sizeof(data_t) * num_components);
-        for(int i=0;i<total;i++) {
+        vector<data_t> nc(num_components);
+
+        for(int i=0;i<num_samples;i++) {
             for(int j=0;j<num_components;j++) {
-                nc[j] += responsibility[i*num_components+j];
+                nc[j] += responsibility[j][i];
             }
         }
 
         for(int j=0;j<num_components;j++) {
-            pi[j] = nc[j]/total;
+            pi[j] = nc[j]/(data_t)num_samples;
         }
 
         for(int j=0;j<num_components;j++) {
             learned_means[j] = 0;
-            for(int i=0;i<total;i++) {
-                learned_means[j] += responsibility[i*num_components+j] * data[i];
+            for(int i=0;i<num_samples;i++) {
+                learned_means[j] += responsibility[j][i] * data[i];
             }
 
             if(nc[j] > 0) {
                 learned_means[j] /= nc[j];
             }
 
-            for(int i=0;i<total;i++) {
+            for(int i=0;i<num_samples;i++) {
                 data_t diff = data[i] - learned_means[j];
-                learned_std[j] += responsibility[i*num_components+j] * diff * diff;
+                learned_std[j] += responsibility[j][i] * diff * diff;
             }
 
             if(nc[j] > 0) {
@@ -208,21 +278,27 @@ void run_gaussian_mixture_1d() {
             learned_std[j] = sqrt(learned_std[j]);
         }
 
-        const int graph_width = 640;
+        /*const int graph_width = 640;
         const int graph_height = 140;
         cv::Mat graph(graph_height, graph_width, CV_8UC3, cv::Scalar(255, 255, 255));
-        draw_1d_data_mixture(graph, data, responsibility, count_per_component, num_components);
+        draw_1d_data_mixture(graph, data, responsibility, samples_per_component, num_components);
         draw_1d_gaussian_mixture(graph, learned_means, learned_std, pi, num_components);
-
-        printf("Iteration %04d\n", iter);
-        for(int j=0;j<num_components;j++) {
-
-            printf("Learned #%d - mean = %f, td = %f, pi =%f\n", j, learned_means[j], learned_std[j], pi[j]);
-        }
 
         char file[100];
         sprintf(file, "./mixture-gaussian-iter%02d.png", iter);
-        cv::imwrite(file, graph);
+        cv::imwrite(file, graph);*/
+
+        cout <<"Iteration = " << iter << endl;
+
+
+        for(int j=0;j<num_components;j++)
+
+            cout << "Learned " << j <<
+                    " - mean = " <<
+                    learned_means[j] <<
+                    ", std = " << learned_std[j] <<
+                    " , pi = " << pi[j] << endl;
+
     }
 
 }
